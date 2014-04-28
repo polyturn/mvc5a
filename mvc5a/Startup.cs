50e756d10ab9c98576dc33bc797a56198dff83ba
polyturn <kleun56@yahoo.com>
2014-04-28 17:36:04 -0400

    initial mvc5a commit

---
+++ mvc5a/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mvc5a.Startup))]
+namespace mvc5a
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
